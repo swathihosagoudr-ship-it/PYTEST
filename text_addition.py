a56e126cc9aea80f6d20a685c308b708ec145c99
swathihosagoudr-ship-it <swathihosagoudr@gmail.com>
2025-12-06 09:50:42 +0530

    Update text_addition.py

--- text_addition.py
+++ text_addition.py
@@ -1,1 +1,7 @@
-
+from addition import add
+def test_add_positive_number():
+  assert add(2,3) == 5
+def test_add_negative_number():
+  assert add(-4, -6) == -10
+def test_add_zero():
+  assert add(0,5) == 5
